2ddcbb2e42d19f06540adb4fd140a0dcc3844d2d
ahangchen <1418659400@qq.com>
2017-02-20 17:51:58 +0800

    id fix

--- mng/static/js/export.js
+++ mng/static/js/export.js
@@ -4,9 +4,9 @@
 function need_clean() {
     var today = new Date();
     if(today.getMonth() == 7 || today.getMonth() == 8 || today.getMonth() == 1 || today.getMonth() == 2) {
-        document.getElementById('clean_form').style.display = 'block';
+        document.getElementById('#clean_form').style.display = 'block';
     } else {
-        document.getElementById('clean_form').style.display = 'none';
+        document.getElementById('#clean_form').style.display = 'none';
     }
 }
 
